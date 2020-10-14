b46dbf8b4a44f3d9254f9fae0e5d551877b17eb9
Kayla Attinello <kaylaattinello@cmail.carleton.ca>
2020-10-14 13:17:42 -0400

    code fixes

--- observer_model.R
+++ observer_model.R
@@ -1,8 +1,6 @@
 library(jagsUI)
 library(tidyverse)
 library(ggmcmc)
-library(rjags)
-
 
 #########################
 #     OBSERVER MODEL    #
@@ -14,7 +12,10 @@ dat_start <- expand.grid(species_obs = c("Red-eyed Vireo","American Robin","Oven
 # set dataframe i can merge
 route <- as.integer(c(1,2,3,4,5,6,7,8,9,10)) # common column
 observer <- c("ID1","ID2","ID3","ID4","ID5","ID6","ID7","ID8","ID9","ID10")
-ecozone <-as.integer(c(14,6,7,13,14,14,9,6,7,13)) # random ecozones for each route
+ecozone <- as.integer(c(2,1,1,2,2,1,2,1,2,1)) # random ecozones for each route
+
+# 6 == ecozone 1
+# 7 == ecozone 2
 
 dat_merge <- data.frame(route, observer, ecozone)
 
@@ -28,11 +29,11 @@ for(i in c("species_obs","observer")){
 nobs <- length(unique(dat_obs$observer_f)) # number of observers
 nroutes <- length(unique(dat_obs$route)) # number of routes
 nspecies_obs <- length(unique(dat_obs$species_obs)) # number of species in observer dataset
-necozones <- length(dat_obs$ecozone) # number of ecozones
+necozones <- length(unique(dat_obs$ecozone)) # number of ecozones
 
-a.sp_eff = runif(nspecies_obs,0.5,2) #coarse mean counts overall
-a.route_eff = rnorm(nroutes,0,0.1) #random variation
-a.ecozone_eff = rnorm(necozones,0,0.1)
+a.sp_eff <- runif(nspecies_obs,0.5,2) #coarse mean counts overall
+a.route_eff <- rnorm(nroutes,0,0.1) #random variation
+a.ecozone_eff <- rnorm(necozones,0,0.1)
 a.obs_off <- runif(nobs,-1,1) # 10 observers
 
 species_effect <- a.sp_eff[dat_obs[i,"species_obs_f"]]
@@ -40,8 +41,10 @@ ecozone_effect <- a.ecozone_eff[dat_obs[i,"ecozone"]]
 route_effect <- a.route_eff[dat_obs[i,"route"]]
 obs_offset <- a.obs_off[dat_obs[i,"observer_f"]]
 
+noise <- 0.1
+
 for(i in 1:nrow(dat_obs)) {
-  int <- obs_offset + route_effect + species_effect + ecozone_effect
+  int <- a.sp_eff + a.route_eff + a.ecozone_eff + a.obs_off
   lmbd_obs <- exp(int+rnorm(1,0,noise))
   dat_obs[i,"count_obs"] <- rpois(1, lambda = lmbd_obs)
 }
@@ -67,11 +70,14 @@ tau_noise_obs <- pow(sd_noise_obs, -2)
   #### observer model
 for(k in 1:ncounts_obs) {
   log(lambda_obs[k]) <- alpha_obs[k] + species_effect[species_obs[k]] + obs_effect[obs[k]] + route_effect[route[k]] + ecozone_effect[ecozone[k]] + noise_obs[k]
+  count_obs[k] ~ dpois(lambda_obs[k])
   
   alpha_obs[k] ~ dnorm(0,0.01) # intercept
   noise_obs[k] ~ dnorm(0, tau_noise_obs)
+  }
   
-  count_obs[k] ~ dpois(lambda[k])
+    for(s in 1:nspecies_obs) {
+    species_effect[s] ~ dnorm(0, 0.01)
   }
   
   for(o in 1:nobs) {
@@ -82,29 +88,26 @@ for(k in 1:ncounts_obs) {
     route_effect[r] ~ dnorm(0, tau_route)
   }
   
-  for(s in 1:nspecies_obs) {
-    species_effect[s] ~ dnorm(0, 0.01)
-  }
-  
   for(e in 1:necozones_obs) {
     ecozone_effect[e] ~ dnorm(0, 0.01)
   }
 
 }
 "
-cat(modl,file = "observer_model.r")
+cat(modl,file = "observer_modelfile.r")
 
 
 
 jags_dat <- list('count_obs' = dat_obs$count_obs,
-                 'obs' = dat_obs$obs,
-                 'species_obs' = dat_obs$species_obs,
+                 'obs' = dat_obs$observer_f,
+                 'species_obs' = dat_obs$species_obs_f,
                  'route' = dat_obs$route,
                  'ecozone' = dat_obs$ecozone,
                  'nobs' = nobs,
                  'ncounts_obs' = ncounts_obs,
                  'nspecies_obs' = nspecies_obs,
-                 'nroutes' = nroutes,
+                 'necozones_obs' = necozones,
+                 'nroutes_obs' = nroutes,
                  'necozones' = necozones)
 
 
@@ -126,13 +129,13 @@ nIter = ceiling( ( (numSavedSteps * thinSteps )+burnInSteps)) # Steps per chain.
 # get posterior samples in mcmc list format ----------------------------
 out = jagsUI(data = jags_dat,
              parameters.to.save = parms,
-             n.chains = 3,
+             n.chains = 4,
              n.burnin = burnInSteps,
              n.thin = thinSteps,
              n.iter = nIter,
              parallel = T,
              modules = NULL,
-             model.file = "observer model.r")
+             model.file = "observer_modelfile.r")
 
 
 summary(out)
