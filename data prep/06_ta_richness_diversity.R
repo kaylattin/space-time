ec61cfca07ef396197c7b03790c0f89cea41ed39
Kayla Attinello <kaylaattinello@cmail.carleton.ca>
2020-12-02 14:51:28 -0500

    working on total abundance / species richness model

--- data prep/06_ta_richness_diversity.R
+++ data prep/06_ta_richness_diversity.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+
+#### MAIN DATASET
 dat <- read.csv("wholedataset_speciesover40_NOV18.csv")
 n_distinct(dat$ObsN)
 
@@ -12,4 +14,28 @@ n_distinct(total$ObsN)
 richness <- dat %>% group_by(space.time, Region, Transect, RouteNumber, Year, ObsN, Forest.cover) %>%
   summarise(Richness = n_distinct(which(Count >= 1))) # how many species present in that route / year combo
 n_distinct(richness$ObsN)
+
+
+write.csv(total, "wholedataset_totalabundance.csv")
+write.csv(richness, "wholedataset_richness.csv")
+
+
+
+
+### OBSERVER DATASET
+obs <- read.csv("observerdataset_NOV12.csv")
+n_distinct(obs$ObsN)
+
+total_obs <- obs %>% group_by(RouteNumber, Year, ObsN, Eco_ID, Obs_ID ) %>%
+  summarise(TotalAbundance = sum(Count))
+n_distinct(total_obs$ObsN)
+
+# dataset for species richness at a transect
+richness_obs <- obs %>% group_by(RouteNumber, Year, ObsN, Eco_ID, Obs_ID ) %>%
+  summarise(Richness = n_distinct(which(Count >= 1))) # how many species present in that route / year combo
+n_distinct(richness_obs$ObsN)
+
+
+write.csv(total_obs, "observerdataset_totalabundance.csv")
+write.csv(richness_obs, "wholedataset_richness.csv")
 
