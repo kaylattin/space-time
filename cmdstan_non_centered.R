d57b0a53b0a01240a9d1310c0f73a11de24e827f
Kayla Attinello <kaylaattinello@cmail.carleton.ca>
2021-03-04 11:24:35 -0500

    added slope calculations

--- cmdstan_non_centered.R
+++ cmdstan_non_centered.R
@@ -2,6 +2,8 @@
 setwd("/Users/kayla/Documents/space-time/final datasets")
 library(tidyverse)
 library(cmdstanr)
+library(shinystan)
+library(rstan)
 
 rm(list = ls())
 gc()
@@ -9,28 +11,26 @@ gc()
 
 d <- read.csv("whole_dataset_over40_ND.csv")
 d_obs <- read.csv("observer_dataset_over40_ND.csv")
-
-
-### cut down dataset to test (first 4 comparison regions)
-d <- d %>% filter(Region %in% c(1,2,3,4,5))
-d_obs <- d_obs %>% filter(ObsN %in% d$ObsN)
-
-
-
-## integer for species and regions
-d$species <- as.integer(as.factor(d$BBL))
-d$reg_id <- as.integer(as.factor(d$ref))
-
+n_distinct(d$SpeciesRegion)
 
 
 ## counts of species per region
-summ <- d %>% group_by(BBL) %>% summarize(nsp = n_distinct(reg_id))
+summ <- d %>% group_by(BBL) %>% summarize(nsp = n_distinct(Region))
 
+
+## create space and time indicators
+d$space <- d$space.time
+d$space[which(d$space == 1)] <- 0
+d$space[which(d$space == 2)] <- 1
+
+d$time <- d$space.time
+d$time[which(d$time == 2)] <- 0
+d$time[which(d$time == 1)] <- 1
 
 
 ### create an indicator ragged array that determines which species are present at which regions
 # it is nreg x nspecies wide
-sp_reg_mat <- as.matrix(read.csv("pseudo_ragged_array_nd.csv"))
+sp_reg_mat <- as.matrix(read.csv("pseudo_ragged_array_nd.csv", header = F))
 
 
 ## set up data -------------------------------
@@ -46,9 +46,8 @@ d_slim <- list(
   count = d$Count,
   space = d$space,
   time = d$time,
-  spacetime = d$space.time,
-  species = d$species, 
-  reg = d$reg_id,
+  species = as.integer(as.factor(d$BBL)),
+  reg = d$Region,
   pforest = as.vector(scale(d$Forest.cover)), # standardized
   obs = as.integer(as.factor(d$ObsN)),
   
@@ -77,28 +76,43 @@ mod <- cmdstan_model(file, pedantic = TRUE)
 # run the model --------------
 fit <- mod$sample(
   data = d_slim,
-  chains = 2,
-  iter_warmup = 500,
-  iter_sampling = 1000,
+  chains = 3,
+  iter_warmup = 1000,
+  iter_sampling = 2000,
   parallel_chains = 2,
   max_treedepth = 12,
-  adapt_delta = 0.9,
-  iter = 0.1,
+  adapt_delta = 0.99,
   show_messages = TRUE,
-  output_dir = "~\space-time\cmdstan output files"
+  init = 0.1,
+  output_dir = "~/space-time/cmdstan_output_files/"
 )
 
-fit$save_object(file = "march2.RDS")
+fit$save_object(file = "march3.RDS")
 fit$cmdstan_diagnose()
 
-# create a stan.fit object 
+
+# quick summary
+summary <- fit$cmdstan_summary()
+
+# create a stanfit S4 object 
 stanfit <- rstan::read_stan_csv(fit$output_files())
+save(stanfit, file =  "march2_stanfit.RDS")
+
+# load up in shinystan for convergence diagnostics
+shinyfit <- as.shinystan(stanfit)
+launch_shinystan(shinyfit)
+
+
+# posterior predictive check
+y_rep <- as.matrix(stanfit, pars = "y_rep")
+ppc_dens_overlay(y = d$Count, yrep = y_rep)
+
+
 
 ## 2nd stage - a priori calculation of space vs. time slopes --------------
+draws <- rstan::extract(stanfit, pars = c("a", "b_space", "b_time"))
 
 
-# dumping code from Adam
-# select for indexes that i want for each biome -> new matrices
 # for each species-reg index, calculate:
 bsl = function(y,x){
   n = length(x)
@@ -110,12 +124,44 @@ bsl = function(y,x){
   return(b)
 }
 
-xx = b_space
-yy = b_time
-bsl(y = yy,x = xx)
 
-## it gives the same estimate as lm
-mlm = lm(yy~xx)
-mlm$coefficients[[2]]
+space_vs_time <- vector("list")
+slope_list <- vector("list")
 
+species = 23
+nreg = 4
+niter = 1000
+
+xx <- array(numeric(), c(niter, nspecies, nreg))
+yy <- array(numeric(), c(niter, nspecies, nreg))
+
+mlm <- rep(list(list()), 23)
+
+
+# create a quick index legend for referencing the slope_list
+species <- rep(1:23, 4)
+reg <- c(rep(1, 23), rep(2, 23), rep(3, 23), rep(4, 23))
+index_legend <- data.frame(species, reg)
+
+
+# extract for each species-reg combo
+ for(s in 1:nspecies){
+    
+    for(g in 1:nreg){   
+      
+      xx[, s, g] = draws$b_space[, s, g]
+      yy[, s, g] = draws$b_time[, s, g]
+      
+      
+      mlm[[s]][[g]] = lm(yy[, s, g] ~ xx[, s, g])
+
+    }
+    
+  }
+
+
+# mlm is an nspecies list of nreg lists, so each species-reg combo has an mlm output
+# calculated from the niterations of space and time
+# so each mlm[[s]][[g]]$coefficients[[2]] is a slope estimate 
+# distribution of slope estimates makes it possible to get the mean slope between time ~ space across all species-reg combos
 
