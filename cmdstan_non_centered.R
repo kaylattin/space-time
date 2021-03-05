7282fdea2631a11db3ffbceaf49c0cce0a6f5003
Kayla Attinello <kaylaattinello@cmail.carleton.ca>
2021-03-05 10:28:55 -0500

    dealing with ragged array in slope calculations

--- cmdstan_non_centered.R
+++ cmdstan_non_centered.R
@@ -13,6 +13,11 @@ d <- read.csv("whole_dataset_over40_ND.csv")
 d_obs <- read.csv("observer_dataset_over40_ND.csv")
 n_distinct(d$SpeciesRegion)
 
+### cut down dataset to test (first 4 comparison regions)
+d <- d %>% filter(Region %in% c(1,2,3,4,5))
+d_obs <- d_obs %>% filter(ObsN %in% d$ObsN)
+
+
 
 ## counts of species per region
 summ <- d %>% group_by(BBL) %>% summarize(nsp = n_distinct(Region))
@@ -32,6 +37,15 @@ d$time[which(d$time == 1)] <- 1
 # it is nreg x nspecies wide
 sp_reg_mat <- as.matrix(read.csv("pseudo_ragged_array_nd.csv", header = F))
 
+# alternatively, create a 0-1 indicator array for species in certain regions
+d$Species <- as.integer(as.factor(d$BBL))
+sp_ind <- d %>% group_by(Region, Species) %>% select(Region, Species)
+sp_ind <- sp_ind[!duplicated(sp_ind),]
+sp_ind$id <- 1
+
+sp_ind_wide <- spread(sp_ind, key = "Region", value = "id", fill = 0)
+sp_ind_wide <- data.frame(sp_ind_wide) %>% select(-Species)
+
 
 ## set up data -------------------------------
 d_slim <- list(
@@ -41,12 +55,12 @@ d_slim <- list(
   nst = 2,
   nobs = length(unique(d$ObsN)),
   nreg_s = summ$nsp,
-  sp_reg_mat =  sp_reg_mat,
+  sp_reg_mat =  sp_ind_wide,
   
   count = d$Count,
   space = d$space,
   time = d$time,
-  species = as.integer(as.factor(d$BBL)),
+  species = d$Species,
   reg = d$Region,
   pforest = as.vector(scale(d$Forest.cover)), # standardized
   obs = as.integer(as.factor(d$ObsN)),
@@ -76,9 +90,9 @@ mod <- cmdstan_model(file, pedantic = TRUE)
 # run the model --------------
 fit <- mod$sample(
   data = d_slim,
-  chains = 3,
-  iter_warmup = 1000,
-  iter_sampling = 2000,
+  chains = 2,
+  iter_warmup = 500,
+  iter_sampling = 1000,
   parallel_chains = 2,
   max_treedepth = 12,
   adapt_delta = 0.99,
@@ -87,18 +101,18 @@ fit <- mod$sample(
   output_dir = "~/space-time/cmdstan_output_files/"
 )
 
-fit$save_object(file = "march3.RDS")
+fit$save_object(file = "march4_test_0.RDS")
 fit$cmdstan_diagnose()
 
 
 # quick summary
-summary <- fit$cmdstan_summary()
+summary <- fit$cmdstan_summary(pars = c())
 
 # create a stanfit S4 object 
 stanfit <- rstan::read_stan_csv(fit$output_files())
-save(stanfit, file =  "march2_stanfit.RDS")
+save(stanfit, file =  "march4_test0_stanfit.RDS")
 
-# load up in shinystan for convergence diagnostics
+# load up in shinystan for convergence diagnostics & posterior predictive / assumptions
 shinyfit <- as.shinystan(stanfit)
 launch_shinystan(shinyfit)
 
@@ -110,9 +124,17 @@ ppc_dens_overlay(y = d$Count, yrep = y_rep)
 
 
 ## 2nd stage - a priori calculation of space vs. time slopes --------------
-draws <- rstan::extract(stanfit, pars = c("a", "b_space", "b_time"))
+draws <- rstan::extract(stanfit, pars = c("a", "b_space", "b_time", "B_TIME", "B_SPACE"))
+b_space <- summary(stanfit, pars = "b_space")
+b_time <- summary(stanfit, pars = "b_time")
+
+x <- print(b_space$summary[1:85])
+y <- print(b_time$summary[1:85])
+
+plot(x, y)
 
 
+  
 # for each species-reg index, calculate:
 bsl = function(y,x){
   n = length(x)
@@ -125,6 +147,8 @@ bsl = function(y,x){
 }
 
 
+biome <- read.csv("forest_biome_input.csv")
+
 space_vs_time <- vector("list")
 slope_list <- vector("list")
 
@@ -132,16 +156,41 @@ species = 23
 nreg = 4
 niter = 1000
 
+
+# how many species and regions in eastern forests?
+east_forest <- biome %>% filter(biome == 1)
+
+d_1 <- d %>% filter(Region %in% east_forest$region)
+
+nspecies_1 <- d_1 %>% distinct(Species)
+nreg_1 <- d_1 %>% distinct(Region)
+
+# how many species and regions in northwestern mountains?
+west_mount <- biome %>% filter(biome == 2)
+
+d_2 <- d %>% filter(Region %in% west_mount$region)
+
+nspecies_2 <- d_2 %>% distinct(Species)
+nreg_1 <- d_2 %>% distinct(Region)
+
+
 xx <- array(numeric(), c(niter, nspecies, nreg))
 yy <- array(numeric(), c(niter, nspecies, nreg))
 
+
+xx_1 <- array(numeric(), c(niter, nspecies_1, nreg_1))
+yy_1 <- array(numeric(), c(niter, nspecies_1, nreg_1))
+
+
+
+xx_2 <- array(numeric(), c(niter, nspecies_2, nreg_2))
+yy_2 <- array(numeric(), c(niter, nspecies_2, nreg_2))
+
+
 mlm <- rep(list(list()), 23)
+mlm_1 <- rep(list(list()), 23)
+mlm_2 <- rep(list(list()), 23)
 
-
-# create a quick index legend for referencing the slope_list
-species <- rep(1:23, 4)
-reg <- c(rep(1, 23), rep(2, 23), rep(3, 23), rep(4, 23))
-index_legend <- data.frame(species, reg)
 
 
 # extract for each species-reg combo
@@ -149,11 +198,40 @@ index_legend <- data.frame(species, reg)
     
     for(g in 1:nreg){   
       
-      xx[, s, g] = draws$b_space[, s, g]
-      yy[, s, g] = draws$b_time[, s, g]
       
-      
-      mlm[[s]][[g]] = lm(yy[, s, g] ~ xx[, s, g])
+      if(sp_reg_mat[s, g] == 1){
+        
+        # slopes for all biomes
+        xx[, s, g] = draws$b_space[, s, g]
+        yy[, s, g] = draws$b_time[, s, g]
+        
+        
+        mlm[[s]][[g]] = lm(yy[, s, g] ~ xx[, s, g])
+        
+        
+        # if region falls in EPA ecoregion 1 == eastern temperate forests
+        if(biome$biome[g] == 1){
+          
+          xx_1[, s, g] = draws$b_space[, s, g]
+          yy_1[, s, g] = draws$b_time[, s, g]
+          
+          
+          mlm_1[[s]][[g]] = lm(yy_1[, s, g] ~ xx_1[, s, g])
+          
+          # if region falls into ecoregion 2 == northwestern forested mountains
+        }else if(biome$biome[g] == 2){
+          
+          xx_2[, s, g] = draws$b_space[, s, g]
+          yy_2[, s, g] = draws$b_time[, s, g]
+          
+          
+          mlm_2[[s]][[g]] = lm(yy_1[, s, g] ~ xx_1[, s, g])
+          
+          
+        }
+        
+      }
+
 
     }
     
@@ -165,3 +243,4 @@ index_legend <- data.frame(species, reg)
 # so each mlm[[s]][[g]]$coefficients[[2]] is a slope estimate 
 # distribution of slope estimates makes it possible to get the mean slope between time ~ space across all species-reg combos
 
+# filter for species-reg combos 
